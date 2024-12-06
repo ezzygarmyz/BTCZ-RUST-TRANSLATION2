119e72796fb03630f1629747f7181fed6e134c5c
jeelybeely <hunterthnelson@hotmail.com>
2024-12-05 22:32:24 -0500

    Add files via upload

---
+++ SRC/lib.rs
@@ -0,0 +1,29 @@
+// src/lib.rs
+
+// Declare existing modules
+pub mod addrman;
+pub mod amount;
+pub mod base58;
+pub mod bloom;
+pub mod chainparams;
+pub mod checkpoints;
+pub mod compat;
+pub mod init;
+pub mod key;
+pub mod main;
+pub mod miner;
+pub mod mruset;
+pub mod net;
+pub mod netbase;
+pub mod rpcserver;
+pub mod script;
+pub mod serialize;
+pub mod sync;
+pub mod txdb;
+pub mod txmempool;
+pub mod ui_interface;
+pub mod util;
+pub mod utilmoneystr;
+pub mod utilstrencodings;
+pub mod version;
+pub mod wallet;
