6d1dd8a3675034d73ba99550ffbe5feada67d4d0
jeelybeely <hunterthnelson@hotmail.com>
2024-12-06 19:32:27 -0500

    Add files via upload

---
+++ SRC/init/init/seeds_main.rs
@@ -0,0 +1,11 @@
+// src/init/seeds_main.rs
+
+/// Mainnet seed nodes for BitcoinZ network.
+/// These nodes help the client bootstrap into the mainnet P2P network.
+pub const SEEDS: [&str; 5] = [
+    "mainnet-seed.btcz.rocks",
+    "mainnet-seed-1.btcz.rocks",
+    "mainnet-seed-2.btcz.rocks",
+    "mainnet-seed-3.btcz.rocks",
+    "mainnet-seed.btcz.io",
+];
