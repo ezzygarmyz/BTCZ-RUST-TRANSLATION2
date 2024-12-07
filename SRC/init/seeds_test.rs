40ead9c0f08127e99d87b34e6fcb86610ef5c855
jeelybeely <hunterthnelson@hotmail.com>
2024-12-06 19:31:30 -0500

    Add files via upload

---
+++ SRC/init/seeds_test.rs
@@ -0,0 +1,9 @@
+// src/init/seeds_test.rs
+
+/// Testnet seed nodes for BitcoinZ network.
+/// These nodes help the client bootstrap into the testnet P2P network.
+pub const SEEDS: [&str; 3] = [
+    "testnet-seed.btcz.rocks",
+    "testnet-seed-1.btcz.rocks",
+    "testnet-seed.btcz.io",
+];
